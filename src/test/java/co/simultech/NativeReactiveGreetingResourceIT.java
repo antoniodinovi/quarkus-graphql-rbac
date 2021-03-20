49375723f7d544ed1ffcca7337f2261d7d99a2f2
quarkusio <no-reply@quarkus.io>
2021-03-20 14:19:34 +0000

    Initial commit

---
+++ src/test/java/co/simultech/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package co.simultech;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
